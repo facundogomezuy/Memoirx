f2645a759c6468febab7b87081b90adc57b070d2
facundogomezuy <facundogomez11564@gmail.com>
2023-07-04 14:08:13 -0300

    Start

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import Header from "./components/Header/Header"
 import "../styles/globals.css"
+import { Analytics } from '@vercel/analytics/react';
+
 export const metadata = {
   title: 'MemoirX',
   description: '',
@@ -11,7 +13,8 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
-        </body>
+        <Analytics />
+      </body>
     </html>
   )
 }
